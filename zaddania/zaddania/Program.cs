1c005579f9837720aea9be2a7363359c2fbcbbb5
Piotr <p.wieliczko@gmail.com>
2023-02-09 12:55:49 +0100

    realizacja zadania domowego - dzień 4

---
+++ zaddania/zaddania/Program.cs
@@ -0,0 +1,48 @@
+﻿// moje zadania
+
+
+string name;
+int age;
+bool sexMale;
+
+name = "Magda";
+age = 37;
+sexMale = false;
+
+//poniżej punkt (1), przykład: Kobieta poniżej 30 lat.
+if (sexMale == true)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("(1) Mężczyzna poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("(1) Mężczyzna powyżej 30 lat");
+    }
+}
+else
+{
+    if (age < 30)
+    {
+        Console.WriteLine("(1) Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("(1) Kobieta powyżej 30 lat");
+    }
+}
+
+//poniżej punkt (2), przykład: Ewa, lat 33.
+Console.WriteLine("(2) " + name + ", lat " + age + ".");
+
+//poniżej punkt (3), przykład: Niepełnoletni mężczyzna. 
+if (age < 18 && sexMale == true)
+{ Console.WriteLine("(3) Niepełnoletni mężczyzna."); }
+else if (age < 18 && sexMale == false)
+{ Console.WriteLine("(3) Niepełnoletnia kobieta."); }
+else if (age >= 18 && sexMale == true)
+{ Console.WriteLine("(3) Pełnoletni mężczyzna."); }
+else { Console.WriteLine("(3) Pełnoletnia kobieta."); }
+
+
